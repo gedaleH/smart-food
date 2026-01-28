abac7e8cb24e08503ddb6602f1c3b088acc9efff
hagede <henri.agede.ext@u-space.fr>
2026-01-28 16:52:57 +0100

    solve list pb

--- app/services/requirement_list_service.py
+++ app/services/requirement_list_service.py
@@ -7,9 +7,9 @@ def generate_list_from_cis(ci_set : set[Ci]):
     requirement_list : list[Requirement] = []
     for ci in ci_set:
         for req in ci.requirements:
-            if any(existing_req.id == req.id for existing_req in requirement_list):
+            if any(existing_req.proposition_id == req.proposition_id for existing_req in requirement_list):
                 for existing_req in requirement_list:
-                    if existing_req.id == req.id:
+                    if existing_req.proposition_id == req.proposition_id:
                         existing_req.quantity += req.quantity
             else:
                 requirement_list.append(copy(req))
